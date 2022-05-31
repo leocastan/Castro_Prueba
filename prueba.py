745e9a24deb0bc4a0ac31fac7ac606722a40e807
lecastro3 <lecastro3@espe.edu.ec>
2022-05-31 08:24:10 -0500

    Avance 2

--- prueba.py
+++ prueba.py
@@ -7,10 +7,11 @@ import pickle
 # Nombre por defecto y ruta donde están los modelos
 app = Flask(__name__)
 
-# Arreglo para almacenar las tareas
+##################### CLIENTE #####################
+# Arreglo para almacenar datos
 registro = []
 
-# 1. Funcion controlador que muestra lista actual de tareas pendientes y un formulario para ingresar un nuevo elemento
+# 1. Funcion controlador que muestra el registro de un cliente
 # Definicion de la ruta por defecto,
 @app.route('/')
 # Lamar a principal
@@ -44,6 +45,50 @@ def enviar():
                 return redirect(url_for('home'))
             else:
                 return redirect(url_for('home'))
+
+
+##################### TIENDA #####################
+# Arreglo para almacenar datos
+registro1 = []
+# 1. Funcion controlador que muestra la tienda
+# Definicion de la ruta por defecto,
+@app.route('/tienda')
+# Lamar a principal
+def tienda():
+    return render_template('tienda.html')
+
+# 2. Funcion controlador para agregar el registro
+# Definicion de la ruta
+@app.route('/enviar1', methods=['POST'])
+# Llamar a enviar
+def enviar1():
+    # Funcion condicional para enviar los datos del formulario
+    if request.method == 'POST':
+
+        nombre = request.form['nombre']
+        telefono = request.form['telefono']
+        estado = request.form['estado']
+        gerente = request.form['estado']
+
+        # Funcion condicional para no registrar en caso de datos vacios
+        if nombre == '' or telefono == '' or estado == '' or gerente == '':
+            #Mensaje de alerta de campos faltantes
+            messagebox.showwarning("¡Alerta!","Ingrese todos los campos")
+            return redirect(url_for('tienda'))
+
+        else:
+            #Mensaje de autorizacion de registro
+            resultado1 = messagebox.askquestion("Registrar", "¿Está seguro que desea registrar los datos?")
+            #Funcion condicional de confirmacion de registro
+            if resultado1 == "yes":
+                registro.append({'nombre': nombre, 'telefono': telefono, 'estado': estado, 'gerente': gerente })
+                return redirect(url_for('tienda'))
+            else:
+                return redirect(url_for('tienda'))
+
+
+
+
 
 # Metodo main del programa
 if __name__ == '__main__':
